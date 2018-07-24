fbc67b49fca7c93a9a8b646d212b0db0e1f39b04
Carlos Tiago <carlostiago88@gmail.com>
2018-07-23 21:29:49 -0300

    removing imports

--- src/main/kotlin/com/kart/classification/service/FileService.kt
+++ src/main/kotlin/com/kart/classification/service/FileService.kt
@@ -1,14 +1,9 @@
 package com.kart.classification.service
 
-import com.kart.classification.domain.Lap
-import com.kart.classification.domain.Pilot
 import java.io.File
 import java.io.IOException
-import java.time.temporal.ChronoUnit
 
 open class FileService {
-    private var laps = mutableListOf<Lap>()
-    private var pilots = arrayListOf<Pilot>()
     private val raceService = RaceService()
 
     fun fileInitialize(filename: String){
@@ -20,7 +15,6 @@ open class FileService {
             if (it.startsWith("Hora").not())
                 raceService.mountDomainByLine(it)
         }
-
         raceService.mountRaceData()
     }
 }
